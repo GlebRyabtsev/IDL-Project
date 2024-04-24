3b8efd6d678c827f64cb1687cfd321e843c0782e
Gleb Ryabtsev <gl.ryabtsev1@gmail.com>
2024-04-23 23:49:05 -0400

    WIP

--- aviary.py
+++ aviary.py
@@ -25,8 +25,10 @@ class Aviary(BaseRLAviary):
                  gui=False,
                  record=False,
                  obs: ObservationType = ObservationType.KIN,
-                 act: ActionType = ActionType.RPM
+                 act: ActionType = ActionType.RPM,
+                 seed: int = 0
                  ):
+
         self.Kp = np.array((-0.2, -0.2, -0.04))
         self.Kd = np.array((-0.06, -0.06, -0.01))
         self.INITIAL_POS_RANGE = ((-1.0, 1.0), (-1.0, 1.0), (-1.0, 1.0))
@@ -37,6 +39,8 @@ class Aviary(BaseRLAviary):
         self.INITIAL_ANG_VEL_RANGE = ((-1.0, 1.0),
                                       (-1.0, 1.0),
                                       (-1.0, 1.0))
+
+        self._rng = np.random.default_rng(seed)
 
         super().__init__(drone_model=drone_model,
                          num_drones=1,
@@ -50,23 +54,96 @@ class Aviary(BaseRLAviary):
                          obs=obs,
                          act=act)
 
+    def _dynamics(self,
+                  rpm,
+                  nth_drone
+                  ):
+        """Explicit dynamics implementation.
+
+        Based on code written at the Dynamic Systems Lab by James Xu.
+
+        Parameters
+        ----------
+        rpm : ndarray
+            (4)-shaped array of ints containing the RPMs values of the 4 motors.
+        nth_drone : int
+            The ordinal number/position of the desired drone in list self.DRONE_IDS.
+
+        """
+        #### Current state #########################################
+        pos = self.pos[nth_drone, :]
+        quat = self.quat[nth_drone, :]
+        vel = self.vel[nth_drone, :]
+        rpy_rates = self.rpy_rates[nth_drone, :]
+        rotation = np.array(p.getMatrixFromQuaternion(quat)).reshape(3, 3)
+        #### Compute forces and torques ############################
+        forces = np.array(rpm ** 2) * self.KF
+        thrust = np.array([0, 0, np.sum(forces)])
+        thrust_world_frame = np.dot(rotation, thrust)
+        force_world_frame = thrust_world_frame - np.array([0, 0, self.GRAVITY])
+        z_torques = np.array(rpm ** 2) * self.KM
+        if self.DRONE_MODEL == DroneModel.RACE:
+            z_torques = -z_torques
+        z_torque = (-z_torques[0] + z_torques[1] - z_torques[2] + z_torques[3])
+        if self.DRONE_MODEL == DroneModel.CF2X or self.DRONE_MODEL == DroneModel.RACE:
+            x_torque = (forces[0] + forces[1] - forces[2] - forces[3]) * (self.L / np.sqrt(2))
+            y_torque = (- forces[0] + forces[1] + forces[2] - forces[3]) * (self.L / np.sqrt(2))
+        elif self.DRONE_MODEL == DroneModel.CF2P:
+            x_torque = (forces[1] - forces[3]) * self.L
+            y_torque = (-forces[0] + forces[2]) * self.L
+        torques = np.array([x_torque, y_torque, z_torque])
+
+        torques_corr, thrust_corr = self._get_corrections()
+        torques += torques_corr
+        # thrust += thrust_corr
+        # hover compensation is corrected in preprocessAction
+        torques = torques - np.cross(rpy_rates, np.dot(self.J, rpy_rates))
+        rpy_rates_deriv = np.dot(self.J_INV, torques)
+        no_pybullet_dyn_accs = force_world_frame / self.M
+        #### Update state ##########################################
+        vel = vel + self.PYB_TIMESTEP * no_pybullet_dyn_accs
+        rpy_rates = rpy_rates + self.PYB_TIMESTEP * rpy_rates_deriv
+        pos = pos + self.PYB_TIMESTEP * vel
+        quat = self._integrateQ(quat, rpy_rates, self.PYB_TIMESTEP)
+        #### Set PyBullet's state ##################################
+        p.resetBasePositionAndOrientation(self.DRONE_IDS[nth_drone],
+                                          pos,
+                                          quat,
+                                          physicsClientId=self.CLIENT
+                                          )
+        #### Note: the base's velocity only stored and not used ####
+        p.resetBaseVelocity(self.DRONE_IDS[nth_drone],
+                            vel,
+                            np.dot(rotation, rpy_rates),
+                            physicsClientId=self.CLIENT
+                            )
+        #### Store the roll, pitch, yaw rates for the next step ####
+        self.rpy_rates[nth_drone, :] = rpy_rates
+
+    def _get_corrections(self):
+        state = self._getDroneStateVector()
+        quat = state[3:7]
+        ang_vel = state[13:16]
+        rot = R.from_quat(quat)
+        rotvec = R.from_quat(quat).as_rotvec()
+        torque = self.Kp * rot.apply(rotvec, inverse=True) + self.Kd * rot.apply(ang_vel, inverse=True)
+
+        z_unit = np.array([0, 0, 1])
+        thrust = self.M * 9.81 / (abs(rot.apply(z_unit).dot(z_unit)))
+
+        return torque, thrust
+
     def _preprocessAction(self, action):
         self.action_buffer.append(action)
-        for k in range(action.shape[0]):  # not really needed since we're using one drone
-            # ignoring act type
-            state = self._getDroneStateVector()
-            quat = state[3:7]
-            ang_vel = state[13:16]
-            rot = R.from_quat(quat)
-            rotvec = R.from_quat(quat).as_rotvec()
-            torque = self.Kp * rot.apply(rotvec, inverse=True) + self.Kd * rot.apply(ang_vel, inverse=True)
+        rpm = np.zeros((1, 4))
+        rpm[0] = np.array(self.HOVER_RPM * (1 + 0.05 * action))  # todo: this coeff is sus
+        return rpm
 
-    def __sample_initial_state(self, seed):
-        rng = np.random.default_rng(seed)
-        rpy = [rng.random() * (upper - lower) + lower for (lower, upper) in self.INITIAL_ANG_RANGE]
-        pos = [rng.random() * (upper - lower) + lower for (lower, upper) in self.INITIAL_POS_RANGE]
-        vel = [rng.random() * (upper - lower) + lower for (lower, upper) in self.INITIAL_VEL_RANGE]
-        ang_vel = [rng.random() * (upper - lower) + lower for (lower, upper) in self.INITIAL_ANG_VEL_RANGE]
+    def _sample_initial_state(self, seed):
+        rpy = [self._rng.random() * (upper - lower) + lower for (lower, upper) in self.INITIAL_ANG_RANGE]
+        pos = [self._rng.random() * (upper - lower) + lower for (lower, upper) in self.INITIAL_POS_RANGE]
+        vel = [self._rng.random() * (upper - lower) + lower for (lower, upper) in self.INITIAL_VEL_RANGE]
+        ang_vel = [self._rng.random() * (upper - lower) + lower for (lower, upper) in self.INITIAL_ANG_VEL_RANGE]
         return pos, rpy, vel, ang_vel
 
     def _housekeeping(self):
@@ -97,13 +174,19 @@ class Aviary(BaseRLAviary):
         #### Load ground plane, drone and obstacles models #########
         self.PLANE_ID = p.loadURDF("plane.urdf", physicsClientId=self.CLIENT)
 
+        pos, rpy, vel, ang_vel = self._sample_initial_state(self._seed)
+
         self.DRONE_IDS = np.array(
             [p.loadURDF(pkg_resources.resource_filename('gym_pybullet_drones', 'assets/' + self.URDF),
-                        self.INIT_XYZS[i, :],
-                        p.getQuaternionFromEuler(self.INIT_RPYS[i, :]),
+                        pos,
+                        p.getQuaternionFromEuler(rpy),
                         flags=p.URDF_USE_INERTIA_FROM_FILE,
                         physicsClientId=self.CLIENT
                         ) for i in range(self.NUM_DRONES)])
+
+        for drone_id in self.DRONE_IDS:
+            p.resetBaseVelocity(drone_id, vel, ang_vel, physicsClientId=self.CLIENT)
+
         #### Remove default damping #################################
         # for i in range(self.NUM_DRONES):
         #     p.changeDynamics(self.DRONE_IDS[i], -1, linearDamping=0, angularDamping=0)
